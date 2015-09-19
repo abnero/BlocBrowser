d3f1c9a12eed771b1fbfc402cf0e1518c84d48f8
abnero <abnero@gmail.com>
2015-09-19 09:02:40 -0700

    Checkpoint 23 Completed

---
+++ BlocBrowser/BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Abner Flores on 9/17/15.
+//  Copyright (c) 2015 Abner Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowser/BlocBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Abner Flores on 9/17/15.
+//  Copyright (c) 2015 Abner Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
