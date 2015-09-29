8a886e67355c7e9f49b9707c9d74552ac25386c2
abnero <abnero@gmail.com>
2015-09-28 21:12:57 -0700

    checkpoint 25 completed

---
+++ BlocBrowser/BlocBrowser/AwesomeFloatingToolbar.h
@@ -0,0 +1,29 @@
+//
+//  AwesomeFloatingToolbar.h
+//  BlocBrowser
+//
+//  Created by Abner Flores on 9/26/15.
+//  Copyright (c) 2015 Abner Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AwesomeFloatingToolbar;
+
+@protocol AwesomeFloatingToolbarDelegate <NSObject>
+
+@optional
+
+-(void) floatingToolbar: (AwesomeFloatingToolbar *) toolbar didSelectButtonWithTitle: (NSString *) title;
+
+@end
+
+@interface AwesomeFloatingToolbar : UIView
+
+-(instancetype) initWithFourTitles: (NSArray *) title;
+
+-(void) setEnabled: (BOOL) enabled forButtonWithTitle: (NSString *) title;
+
+@property (nonatomic, weak) id <AwesomeFloatingToolbarDelegate> delegate;
+
+@end
